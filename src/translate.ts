ec6706d88d81d96895a3fb1038b7030fcda1fdbf
yizhu <chenzhicheng1994@gmail.com>
2022-11-25 20:34:18 +0800

    init

---
+++ src/translate.ts
@@ -0,0 +1,3 @@
+export function translate(script: string): Function {
+    return () => ([]);
+}
---
+++ test/translate.test.ts
@@ -0,0 +1,45 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import { translate } from '../src/translate';
+
+describe('translate', () => {
+    let sequelize, User;
+
+    beforeAll(async () => {
+        sequelize = new Sequelize('sqlite::memory:');
+
+        User = sequelize.define('User', {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            firstName: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            lastName: {
+                type: DataTypes.STRING
+            }
+        }, {
+            tableName: 'user'
+        });
+
+        await sequelize.sync({ force: true });
+    });
+
+    afterEach(async () => {
+        await sequelize.drop();
+    });
+
+    describe('select', () => {
+        beforeAll(async () => {
+            await User.create({ firstName: 'Jane', lastName: 'Gate' });
+            await User.create({ firstName: 'Bob' });
+        });
+        test('translate select where equal', async () => {
+            const func: Function = translate('select * from user where id = 0;');
+            const result :Array<any> = func({ user: User });
+            expect(result).toBe([{ id: 0, firstName: 'Jane', lastName: 'Gate' }]);
+        });
+    });
+});
