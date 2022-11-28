82009788df1084ed6b3fa2b32acbfe8f1c1e7f20
yizhu <chenzhicheng1994@gmail.com>
2022-11-28 20:16:25 +0800

    remove useless files

--- src/utils/clean.ts
+++
@@ -1,9 +0,0 @@
-export function clean(script: string): string {
-    return script
-        .replace(/[\r\n\t]+/g,' ')
-        .replace(/([(),;])/g, ' $1 ')
-        .replace(/\s{2,}/g, ' ')
-        .replace(/["']/g, '')
-        .trim()
-        .toLowerCase();
-}
